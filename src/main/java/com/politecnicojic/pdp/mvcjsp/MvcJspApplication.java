f6c99f0d3d858cbfeb153f89235018c9a9c4158b
Andrés Alarcón Guerrero <andresf.alarcon@gmail.com>
2019-05-21 12:19:19 -0500

    Proyecto Inicial

---
+++ src/main/java/com/politecnicojic/pdp/mvcjsp/MvcJspApplication.java
@@ -0,0 +1,13 @@
+package com.politecnicojic.pdp.mvcjsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcJspApplication.class, args);
+	}
+
+}
